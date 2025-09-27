ae0e6ef395d48fbfa1a8ed20d309ad1e2c45facd
Jose Maria Romero Tendero <josemaria.romero4@alu.uclm.es>
2025-09-27 13:30:02 +0200

    Creación de la clase Modelo (#12)
    Co-authored-by: usua <usuario@JRTX5>

---
+++ src/AppForSEII2526.API/Models/Modelo.cs
@@ -0,0 +1,21 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AppForSEII2526.API.Models
+{
+   
+
+    public class Modelo
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string NameModel { get; set; }
+
+
+
+
+    }
+}
