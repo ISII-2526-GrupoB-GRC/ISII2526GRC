6219ae0f5608ca8cf5ad05e05b41e721bfa671de
Jose Maria Romero Tendero <josemaria.romero4@alu.uclm.es>
2025-09-27 17:02:11 +0200

    Creacion de la clase PurchasedItem (#16)
    Co-authored-by: usua <usuario@JRTX5>

---
+++ src/AppForSEII2526.API/Models/PurchaseItem.cs
@@ -0,0 +1,24 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace AppForSEII2526.API.Models
+{
+    public class PurchaseItem
+    {
+        public string? Description { get; set; }
+
+        [Required]
+        [ForeignKey("Device")]
+        public int DeviceId { get; set; }
+
+        [Required]
+        public double Price { get; set; }
+
+        [Required]
+        [ForeignKey("Purchase")]
+        public int PurchaseID { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
