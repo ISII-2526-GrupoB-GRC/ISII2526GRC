ad78c712c0c38a05d0a1f1c28b31a8437432ceb9
Rodrigo Díaz Quintanar <rdiazquintanar@gmail.com>
2025-09-30 22:59:49 +0200

    Clase Scale Creada (#20)

---
+++ src/AppForSEII2526.API/Models/Scale.cs
@@ -0,0 +1,24 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Scale
+    {
+        //Primary key
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is Scale scale)
+            {
+                return Id == scale.Id && Name == scale.Name;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Name);
+        }
+    }
+}
