7eb84ef322850a7491df5a339b473b828d432fc7
Rodrigo Díaz Quintanar <rdiazquintanar@gmail.com>
2025-09-30 22:59:39 +0200

    Clase Repair Creada (#22)

---
+++ src/AppForSEII2526.API/Models/Repair.cs
@@ -0,0 +1,34 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class Repair
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string? Description { get; set; }
+        [Required]
+        public float Cost { get; set; }
+        [Required]
+        public string? Name { get; set; }
+        [Required]
+        [ForeignKey("Scale")]
+        public int ScaleId { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is Repair repair)
+            {
+                return Id == repair.Id &&
+                       Description == repair.Description &&
+                       Cost == repair.Cost &&
+                       Name == repair.Name &&
+                       ScaleId == repair.ScaleId;
+            }
+            return false;
+        }
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id, Description, Cost, Name, ScaleId);
+        }
+    }
+}
