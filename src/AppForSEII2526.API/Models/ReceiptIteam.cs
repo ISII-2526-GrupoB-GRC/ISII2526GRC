00ba29063695881408555aee1e4975360e7bb76d
Rodrigo Díaz Quintanar <rdiazquintanar@gmail.com>
2025-09-30 22:59:22 +0200

    Clase ReceiptIteam creada (#26)

---
+++ src/AppForSEII2526.API/Models/ReceiptIteam.cs
@@ -0,0 +1,29 @@
+﻿namespace AppForSEII2526.API.Models
+{
+    public class ReceiptIteam
+    {
+        [Key]
+        [Required]
+        public string? Model { get; set; }
+        [Required]
+        [ForeignKey("Repair")]
+        public int RepairId { get; set; }
+        [Required]
+        [ForeignKey("Receipt")]
+        public int ReceiptId { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            if (obj is ReceiptIteam receiptIteam)
+            {
+                return Model == receiptIteam.Model && RepairId == receiptIteam.RepairId && ReceiptId == receiptIteam.ReceiptId;
+            }
+            return false;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Model, RepairId, ReceiptId);
+        }
+    }
+}
