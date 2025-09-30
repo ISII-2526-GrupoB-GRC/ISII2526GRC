e0390d8423708fba252e586e30cfa76331391c4e
Guillermo Rosillo Serrano <Guillermo.Rosillo@alu.uclm.es>
2025-09-30 22:59:05 +0200

    Clase Device creada (#27)

---
+++ src/AppForSEII2526.API/Models/Device.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Device
+{
+	public Device()
+	{
+		public string Brand { get; set; }
+		public string Color { get; set; }
+		public int Id { get; set; }
+		public string Name { get; set; }
+		public double PriceForPurchace { get; set; }
+		public string PriceForRent { get; set; }
+		public List<PurchaseItems> PurchaseItems { get; set; }
+		public QualityType Quality { get; set; }
+		public int quanityForPurchase { get; set; }
+		public int quantityForRent { get; set;}
+		public List<ReviewItems> ReviewItems { get; set; }
+		public int Year { get; set; }
+	}
+}
