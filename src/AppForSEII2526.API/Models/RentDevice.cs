4f1d2ee934977915e4438a2d5743b0fd0fd70aa0
GUILLERMO ROSILLO SERRANO <Guillermo.Rosillo@alu.uclm.es>
2025-09-30 23:06:57 +0200

    Definición de atributos clase RentDevice

---
+++ src/AppForSEII2526.API/Models/RentDevice.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+public class RentDevice
+{
+	public RentDevice()
+	{
+		public int DeviceId { get; set; }
+	    public double price { get; set; }
+	    public int Quantity { get; set; }
+	    public int RentId { get; set; }
+	}
+}
