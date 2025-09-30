58eb032cbee6baf7c04a7a442395f298e57f71ef
GUILLERMO ROSILLO SERRANO <Guillermo.Rosillo@alu.uclm.es>
2025-09-30 23:01:56 +0200

    Definición de atributos de la clase Rental

---
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+public class Rental
+{
+	public Rental()
+	{
+		public string DeliveryAdress {  get; set; }
+	    public int Id { get; set; }
+		public string Name { get; set; }
+		public PaymentMethod PaymentMethod { get; set; }
+		public DateTime RentalDate { get; set; }
+		public DateTime RentalDateFrom { get; set; }
+		public DateTime RentalDateTo { get; set; }
+		public string Surname { get; set; }
+		public double TotalPrice { get; set; }
+	}
+}
