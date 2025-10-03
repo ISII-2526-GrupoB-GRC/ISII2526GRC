154262ec9fde2e476fab04cc48720a622edc6264
Jose Maria Romero Tendero <josemaria.romero4@alu.uclm.es>
2025-10-03 15:15:01 +0200

    Añadir relacion entre clase Purchase, Purchaseitem, Model, Device (#36)
    Añado los constructores (vacios y con parametros) a las clases Purchase, PurchaseItem y Modelo
    
    Co-authored-by: usua <usuario@JRTX5>

--- src/AppForSEII2526.API/Models/Device.cs
+++ src/AppForSEII2526.API/Models/Device.cs
@@ -10,11 +10,16 @@ public class Device
 		public string Name { get; set; }
 		public double PriceForPurchace { get; set; }
 		public string PriceForRent { get; set; }
-		public List<PurchaseItems> PurchaseItems { get; set; }
+		public List<PurchaseItem> PurchaseItems { get; set; }
 		public QualityType Quality { get; set; }
 		public int quanityForPurchase { get; set; }
 		public int quantityForRent { get; set;}
 		public List<ReviewItems> ReviewItems { get; set; }
 		public int Year { get; set; }
-	}
+
+		public Modelo Model { get; set; } // Relacion con Modelo
+
+
+
+}
 }
