d1ffca2afe862ed669b928bacb943be7c8d4bee5
Guillermo Rosillo Serrano <Guillermo.Rosillo@alu.uclm.es>
2025-10-03 13:29:30 +0200

    Definición de atributos clase RentDevice (#29)
    * Definición de atributos clase RentDevice
    
    * Definición de atributos clase RentDevice
    
    * Creada clase RentDevice con atributos, además de constructor vacío y completo

---
+++ src/AppForSEII2526.API/Models/RentDevice.cs
@@ -0,0 +1,32 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+public class RentDevice
+{
+	[Required]
+	[ForeignKey("Device")]
+	public int DeviceId { get; set; }
+
+	[Required]
+	public double Price { get; set; }
+
+	[Required]
+	public int Quantity { get; set; }
+
+	[Required]
+	[ForeignKey("Rental")]
+	public int RentId { get; set; }
+
+	//Constructores
+
+	public RentDevice() { }
+
+	public RentDevice(int deviceID, double price, int quantity, int rentId)
+	{
+		DeviceID = deviceID;
+		Price = price;
+		Quantity = quantity;
+		RentId = rentId;
+	}
+
+}
