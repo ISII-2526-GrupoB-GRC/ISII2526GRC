fc69e7308d8af85f75902bdb4aaffa65da35e579
GUILLERMO ROSILLO SERRANO <Guillermo.Rosillo@alu.uclm.es>
2025-10-03 14:11:39 +0200

    Creación de la clase Rental con sus atributos y los constructores vacío y completo

--- src/AppForSEII2526.API/Models/Rental.cs
+++ src/AppForSEII2526.API/Models/Rental.cs
@@ -1,17 +1,56 @@
-﻿using System;
+﻿System.ComponentModel.DataAnnotations;
+System.ComponentModel.DataAnnotations.Schema;
 
-public class Rental
+namespace AppForSEII2526.API.Models
 {
-	public Rental()
-	{
-		public string DeliveryAdress {  get; set; }
-	    public int Id { get; set; }
-		public string Name { get; set; }
-		public PaymentMethod PaymentMethod { get; set; }
-		public DateTime RentalDate { get; set; }
-		public DateTime RentalDateFrom { get; set; }
-		public DateTime RentalDateTo { get; set; }
-		public string Surname { get; set; }
-		public double TotalPrice { get; set; }
-	}
+    public class Rental
+    {
+        [Required]
+        [StringLength(200, ErrorMessage = "Máximo número de caracteres alcanzado (200)", MinimumLength = 1)]
+        public string DeliveryAdress { get; set; }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Máximo número de caracteres alcanzado (100)", MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "Máximo número de caracteres alcanzado (100)", MinimumLength = 1)]
+        public string Surname { get; set; }
+
+        public PaymentMethod PaymentMethod { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RentalDate { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RentalDateFrom { get; set; }
+
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime RentalDateTo { get; set; }
+
+        public double TotalPrice { get; set; }
+
+        // Constructores:
+
+        public Rental() { }
+
+        public Rental(string deliveryAdress, int id, int name, int surname, DateTime rentalDate, DateTime rentalDateFrom, DateTime rentalDateTo, double totalPrice)
+        {
+            this.DeliveryAdress = deliveryAdress;
+            this.Id = id;
+            this.Name = name;
+            this.surname = surname;
+            this.RentalDate = rentalDate; ;
+            this.RentalDateFrom = rentalDateFrom;
+            this.RentalDateTo = rentalDateTo;
+            this.TotalPrice = totalPrice;
+        }
+
+    }
 }
