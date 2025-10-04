ffa3c508460a44e087140aa1d339d1997c3a5d2b
usua <usuario@JRTX5>
2025-10-04 12:49:35 +0200

    He arreglado los bugs de los constructores, añadiendo a los constructores los objetos de las relaciones.

--- src/AppForSEII2526.API/Models/Modelo.cs
+++ src/AppForSEII2526.API/Models/Modelo.cs
@@ -17,9 +17,10 @@ namespace AppForSEII2526.API.Models
         public List<Device> Devices { get; set; } //Relacion con Device
 
         public Modelo() { } //constructor vacio
-        public Modelo(int id, string nameModel) { //constructor con parametros
+        public Modelo(int id, string nameModel, IList<Device> devices) { //constructor con parametros
             this.Id = id;
             this.NameModel = nameModel;
+            this.Devices = (List<Device>)devices;
 
         }
 
