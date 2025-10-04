637f54f938647c5c0350975460a0b14be2d49c45
usua <usuario@JRTX5>
2025-10-04 12:57:55 +0200

    He añadido los atributos de mis clases para hacer la migracion

--- src/AppForSEII2526.API/Data/ApplicationDbContext.cs
+++ src/AppForSEII2526.API/Data/ApplicationDbContext.cs
@@ -5,4 +5,9 @@ using AppForSEII2526.API.Models;
 namespace AppForSEII2526.API.Data;
 
 public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options) {
+
+    public DbSet<Modelo> Modelo { get; set; }
+    public DbSet<Purchase> Purchase { get; set; }
+    public DbSet<PurchaseItem> PurchaseItem { get; set; }
+    public DbSet<Device> Device { get; set; }
 }
