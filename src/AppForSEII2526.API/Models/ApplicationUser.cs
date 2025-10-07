11b08855a0e5015c2f33956f3ff32c0fcf7a4767
Jose Maria Romero Tendero <josemaria.romero4@alu.uclm.es>
2025-10-07 11:06:32 +0200

    He puesto los atributos de la clase aplicationuser (#54)
    Co-authored-by: usua <usuario@JRTX5>

--- src/AppForSEII2526.API/Models/ApplicationUser.cs
+++ src/AppForSEII2526.API/Models/ApplicationUser.cs
@@ -4,4 +4,39 @@ namespace AppForSEII2526.API.Models;
 
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser {
+
+    public ApplicationUser() { }
+
+
+    public ApplicationUser(string id, string name, string surname, string userName)
+    {
+        this.Id = id;
+        this.Name = name;
+        this.Surname = surname;
+        this.UserName = userName;
+        this.Email = userName;
+    }
+
+    [Display(Name = "Name")]
+    public string Name
+    {
+        get;
+        set;
+    }
+
+    [Display(Name = "Surname")]
+    public string Surname
+    {
+        get;
+        set;
+    }
+
+    [Display(Name = "Country")]
+    public string Country {
+        get;
+        set;
+    }
+
+
+
 }
