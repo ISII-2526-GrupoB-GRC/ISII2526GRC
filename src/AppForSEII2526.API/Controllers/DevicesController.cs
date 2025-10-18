915f9e50adddbd6f961deb918ac05bc09219506c
rodrivva <rdiazquintanar@gmail.com>
2025-10-18 12:34:31 +0200

    Creado controlador, carpeta DTOs con RepairDTO

---
+++ src/AppForSEII2526.API/Controllers/DevicesController.cs
@@ -0,0 +1,64 @@
+﻿using AppForSEII2526.API.DTOs.RepairDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DevicesController : ControllerBase
+    {
+        //used to enable your controller to access to the database
+        private readonly ApplicationDbContext _context;
+
+        //used to log any information when your system is running
+        private readonly ILogger<DevicesController> _logger;
+
+        public DevicesController(ApplicationDbContext context, ILogger<DevicesController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(decimal), (int)HttpStatusCode.OK)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.BadRequest)]
+        public async Task<ActionResult> ComputeDivision(decimal op1, decimal op2)
+        {
+            if (op2 == 0)
+            {
+                _logger.LogError($"{DateTime.Now} Exception: op2=0, division by 0");
+                return BadRequest("op2 must be different from 0");
+            }
+            decimal result = decimal.Round(op1 / op2, 2);
+            return Ok(result);
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<Device>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetDevices()
+        {
+            IList<Device> devices = await _context.Device.ToListAsync();
+            return Ok(devices);
+
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<RepairDTO>), (int)HttpStatusCode.OK)]
+        public async Task<ActionResult> GetRepairs()
+        {
+            var Repairs = await _context.Repair
+                .Select(r => new RepairDTO(
+                    r.Id,
+                    r.Description,
+                    r.Cost,
+                    r.Name,
+                    r.ScaleId
+                )).ToListAsync();
+            return Ok(Repairs); 
+        }
+    }
+}
