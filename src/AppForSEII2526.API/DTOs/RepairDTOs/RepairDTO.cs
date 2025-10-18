49855b1fb3951fc8769efa02106dc9df15128052
rodrivva <rdiazquintanar@gmail.com>
2025-10-18 12:34:31 +0200

    Creado controlador, carpeta DTOs con RepairDTO

---
+++ src/AppForSEII2526.API/DTOs/RepairDTOs/RepairDTO.cs
@@ -0,0 +1,22 @@
+﻿namespace AppForSEII2526.API.DTOs.RepairDTOs
+{
+    public class RepairDTO
+    {
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public float Cost { get; set; }
+        public string Name { get; set; }
+        public int ScaleId { get; set; }
+
+        // Constructor con parámetros
+        public RepairDTO(int id, string? description, float cost, string name, int scaleId)
+        {
+            Id = id;
+            Description = description;
+            Cost = cost;
+            Name = name;
+            ScaleId = scaleId;
+        }
+
+    }
+}
