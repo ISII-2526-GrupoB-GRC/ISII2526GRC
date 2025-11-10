7412deb2a1e9bb5a62e869c17a373322dcda5a73
usua <usuario@JRTX5>
2025-11-10 12:05:04 +0100

    Arreglo bugs clase purchase

--- src/AppForSEII2526.API/Models/Purchase.cs
+++ src/AppForSEII2526.API/Models/Purchase.cs
@@ -3,7 +3,7 @@ using Microsoft.Identity.Client;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Data;
-using static AppForSEII2526.API.Models.PaymentMethod;
+using static AppForSEII2526.API.Models.PaymentMethodTypes;
 
 namespace AppForSEII2526.API.Models
 {
@@ -11,7 +11,7 @@ namespace AppForSEII2526.API.Models
     {
 
         [Required(ErrorMessage = "El nombre de usuario es obligatorio")]
-        [StringLength(10,ErrorMessage = "El nombre de usuario no puede ser mas largo de 1 caracteres")]
+        [StringLength(10, ErrorMessage = "El nombre de usuario no puede ser mas largo de 1 caracteres")]
         public string CustomerUserName { get; set; }
 
         [Required(ErrorMessage = "El apellido/s del cliente es obligatorio/s")]
@@ -24,7 +24,7 @@ namespace AppForSEII2526.API.Models
         public string DeliveryAddress { get; set; }
 
         public int Id { get; set; }
-        
+
         [Required(ErrorMessage = "La forma de pago es obligatoria introducirla")]
         public PaymentMethodTypes PaymentMethod { get; set; } //modificar
 
@@ -33,13 +33,13 @@ namespace AppForSEII2526.API.Models
         [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
         public DateTime PurchaseDate { get; set; }
 
-        [Required(ErrorMessage ="El precio total de compra es obligatorio introducirlo")]
-        [Precision(10,2)]
+        [Required(ErrorMessage = "El precio total de compra es obligatorio introducirlo")]
+        [Precision(10, 2)]
 
         public double TotalPrice { get; set; }
 
         [Required(ErrorMessage = "La cantidad minima en una compra debe de ser de al meno 1 dispositivo.")]
-        [Range(1,int.MaxValue,ErrorMessage = "La cantidad minima en una compra debe de ser de al meno 1 dispositivo.")]
+        [Range(1, int.MaxValue, ErrorMessage = "La cantidad minima en una compra debe de ser de al meno 1 dispositivo.")]
         public int TotalQuanty { get; set; }
 
         public IList<PurchaseItem> PurchaseItems { get; set; } //Relacion con PurchaseItem
@@ -49,20 +49,33 @@ namespace AppForSEII2526.API.Models
         public Purchase() { } //constructor vacio
 
 
-        public Purchase(string CustomerUserName, string CustomerUserSurname, string DeliveryAddress, int id, PaymentMethodTypes paymentMethod, DateTime purchaseDate, double totalPrice, int totalQuanty, IList<PurchaseItem> purchasesItems) //constructor con parametros
+        public Purchase(string CustomerUserName, string CustomerUserSurname, string DeliveryAddress, PaymentMethodTypes paymentMethod, DateTime purchaseDate, double totalPrice, int totalQuanty, IList<PurchaseItem> purchasesItems, ApplicationUser user) //constructor con parametros
         {
             this.CustomerUserName = CustomerUserName;
             this.CustomerUserSurname = CustomerUserSurname;
             this.DeliveryAddress = DeliveryAddress;
-            this.Id = id;
+            //this.Id = id;
             this.PaymentMethod = paymentMethod;
             this.PurchaseDate = purchaseDate;
             this.TotalPrice = totalPrice;
             this.TotalQuanty = totalQuanty;
             this.PurchaseItems = (IList<PurchaseItem>)purchasesItems;
+            this.ApplicationUser = user;
         }
 
-
-        
+        public override bool Equals(object? obj)
+        {
+            return obj is Purchase purchase &&
+                   CustomerUserName == purchase.CustomerUserName &&
+                   CustomerUserSurname == purchase.CustomerUserSurname &&
+                   DeliveryAddress == purchase.DeliveryAddress &&
+                   Id == purchase.Id &&
+                   PaymentMethod == purchase.PaymentMethod &&
+                   PurchaseDate == purchase.PurchaseDate &&
+                   TotalPrice == purchase.TotalPrice &&
+                   TotalQuanty == purchase.TotalQuanty &&
+                   EqualityComparer<IList<PurchaseItem>>.Default.Equals(PurchaseItems, purchase.PurchaseItems) &&
+                   EqualityComparer<ApplicationUser>.Default.Equals(ApplicationUser, purchase.ApplicationUser);
+        }
     }
 }
