284db95ee4e82b87102ecc8bd0b2824a4279b26f
usua <usuario@JRTX5>
2025-11-12 20:38:16 +0100

    implemento el codigo de la clase device for purchase

--- src/AppForSEII2526.API/DTOs/DeviceDTOs/DeviceForPurchaseDTO.cs
+++ src/AppForSEII2526.API/DTOs/DeviceDTOs/DeviceForPurchaseDTO.cs
@@ -1,6 +1,39 @@
-﻿namespace AppForSEII2526.API.DTOs.DeviceDTOs
+﻿
+namespace AppForSEII2526.API.DTOs.DeviceDTOs
 {
     public class DeviceForPurchaseDTO
     {
+        public string name { get; set; }
+        public string Brand { get; set; }
+        public string nameModel { get; set; }
+        public string colour { get; set; }
+        public double price { get; set; }
+
+        public DeviceForPurchaseDTO(string name, string marca, string nameModel, string colour, double price)
+        {
+            this.name = name;
+            Brand = marca;
+            this.nameModel = nameModel;
+            this.colour = colour;
+            this.price = price;
+        }
+
+
+        public override bool Equals(object? obj)
+        {
+            if (obj is DeviceForPurchaseDTO other)
+            {
+                return name == other.name &&
+                       Brand == other.Brand &&
+                       nameModel == other.nameModel &&
+                       colour == other.colour &&
+                       price == other.price;
+
+
+            }
+            else return false;
+        }
+
+
     }
 }
