e4e9bc7fc446e7afe1bc5bdd2a7ab29567e80fa7
rodrivva <rdiazquintanar@gmail.com>
2025-11-13 18:51:14 +0100

    Creado DTO de ReceiptItem

---
+++ src/AppForSEII2526.API/DTOs/ReceiptDTOs/ReceiptItemDTO.cs
@@ -0,0 +1,32 @@
+﻿namespace AppForSEII2526.API.DTOs.ReceiptDTOs
+{
+    public class ReceiptItemDTO
+    {
+        public string Name { get; set; }
+        public string Scale { get; set; }
+        public decimal Cost { get; set; }
+        public string Model { get; set; }
+        public ReceiptItemDTO() { }
+        public ReceiptItemDTO(string Name, string Scale, decimal Cost, string Model)
+        {
+            this.Name = Name;
+            this.Scale = Scale;
+            this.Cost = Cost;
+            this.Model = Model;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is ReceiptItemDTO dTO &&
+                   Name == dTO.Name &&
+                   Scale == dTO.Scale &&
+                   Cost == dTO.Cost &&
+                   Model == dTO.Model;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Name, Scale, Cost, Model);
+        }
+    }
+}
