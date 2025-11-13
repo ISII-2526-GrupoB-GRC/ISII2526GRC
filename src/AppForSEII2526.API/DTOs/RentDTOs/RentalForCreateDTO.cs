c10625a2ba22500b3e9108e11c50f722c6129689
GUILLERMO ROSILLO SERRANO <Guillermo.Rosillo@alu.uclm.es>
2025-11-13 19:39:26 +0100

    RentalForCreateDTO creado para el método post del caso de uso Alquilar Dispositivo.

---
+++ src/AppForSEII2526.API/DTOs/RentDTOs/RentalForCreateDTO.cs
@@ -0,0 +1,45 @@
+﻿namespace AppForSEII2526.API.DTOs.RentDTOs
+{
+    public class RentalForCreateDTO
+    {
+        //Apartado 5
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Surname { get; set; }
+        [Required]
+        public string DeliveryAddress { get; set; }
+        public PaymentMethodTypes PaymentMethod { get; set; }
+
+        public DateTime RentalDateTo { get; set; }
+        public DateTime RentalDateFrom { get; set; }
+        public IList<RentalItemDTO> RentalItems { get; set; }
+
+        public RentalForCreateDTO() { }
+
+        public RentalForCreateDTO(string name, string surname, string deliveryAddress, PaymentMethodTypes paymentMethod, DateTime rentalDateFrom, DateTime rentalDateTo, IList<RentalItemDTO> rentalItems)
+        {
+
+            Name = name;
+            Surname = surname;
+            DeliveryAddress = deliveryAddress;
+            PaymentMethod = paymentMethod;
+            RentalDateFrom = rentalDateFrom;
+            RentalDateTo = rentalDateTo;
+            RentalItems = rentalItems;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return obj is RentalForCreateDTO dTO &&
+                   Name == dTO.Name &&
+                   Surname == dTO.Surname &&
+                   DeliveryAddress == dTO.DeliveryAddress &&
+                   PaymentMethod == dTO.PaymentMethod &&
+                   RentalDateTo == dTO.RentalDateTo &&
+                   RentalDateFrom == dTO.RentalDateFrom &&
+                   EqualityComparer<IList<RentalItemDTO>>.Default.Equals(RentalItems, dTO.RentalItems);
+        }
+
+    }
+}
