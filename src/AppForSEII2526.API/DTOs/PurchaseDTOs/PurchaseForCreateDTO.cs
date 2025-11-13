464219c4aa4f0c3aa86dd963f7efcbd2985ec323
usua <usuario@JRTX5>
2025-11-13 18:54:37 +0100

    implementacion del codigo de la clase purchaseForCreateDTO

--- src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseForCreateDTO.cs
+++ src/AppForSEII2526.API/DTOs/PurchaseDTOs/PurchaseForCreateDTO.cs
@@ -1,6 +1,44 @@
-﻿namespace AppForSEII2526.API.DTOs.PurchaseDTOs
+﻿using static AppForSEII2526.API.Models.PaymentMethodTypes;
+
+namespace AppForSEII2526.API.DTOs.PurchaseDTOs
 {
     public class PurchaseForCreateDTO
     {
+        public IList<PurchaseItemDTO> purchaseItems { get; set; }
+        public string name { get; set; }
+        //public int id { get; set; }
+        public string surnames { get; set; }
+        public string deliveryAddress { get; set; }
+        public PaymentMethodTypes paymentMethod { get; set; }
+
+        public PurchaseForCreateDTO(IList<PurchaseItemDTO> purchaseItems, string name, string surnames, string deliveryAddress, PaymentMethodTypes paymentMethod)
+        {
+            this.purchaseItems = purchaseItems;
+            this.name = name;
+            //this.id = id;
+            this.surnames = surnames;
+            this.deliveryAddress = deliveryAddress;
+            this.paymentMethod = paymentMethod;
+        }
+        public PurchaseForCreateDTO()
+        {
+            purchaseItems = new List<PurchaseItemDTO>();
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if (obj is PurchaseForCreateDTO other)
+            {
+                return this.purchaseItems.SequenceEqual(other.purchaseItems) &&
+                       this.name == other.name &&
+                       this.surnames == other.surnames &&
+                       this.deliveryAddress == other.deliveryAddress &&
+                       this.paymentMethod == other.paymentMethod;
+                // this.id == other.id;
+            }
+            else return false;
+
+        }
     }
 }
