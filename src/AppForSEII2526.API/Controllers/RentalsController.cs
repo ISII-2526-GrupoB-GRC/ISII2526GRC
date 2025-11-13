5e843e64bdf01ac3f18b09d01c2b76c0fb7496c0
GUILLERMO ROSILLO SERRANO <Guillermo.Rosillo@alu.uclm.es>
2025-11-13 19:56:34 +0100

    Creación del método post/ create en el controlador RentalsController

--- src/AppForSEII2526.API/Controllers/RentalsController.cs
+++ src/AppForSEII2526.API/Controllers/RentalsController.cs
@@ -75,7 +75,117 @@ namespace AppForSEII2526.API.Controllers
             return Ok(rental);
         }
 
+        // Método Create / Post
 
+        [HttpPost]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(RentalDetailDTO), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ValidationProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(string), (int)HttpStatusCode.Conflict)]
+        public async Task<ActionResult> CreateRental(RentalForCreateDTO rentalForCreate)
+        {
+            if (rentalForCreate.Name == null) ModelState.AddModelError("Name", "El campo Nombre es obligatorio");
 
+            var user = _context.ApplicationUser.FirstOrDefault(au => au.UserName == rentalForCreate.Name);
+            if (user == null) ModelState.AddModelError("RentalApplicationUser", "Error! UserName no registrado");
+
+            if (rentalForCreate.Surname == null) ModelState.AddModelError("Surname", "El campo Apellido es obligatorio");
+
+            if (rentalForCreate.DeliveryAddress == null) ModelState.AddModelError("DeliveryAddress", "El campo Dirección es obligatorio");
+
+            if (rentalForCreate.RentalDateFrom <= DateTime.Today)
+                ModelState.AddModelError("RentalDateFrom", "Error! La fecha de alquiler debe comenzar más tarde de hoy");
+
+            if (rentalForCreate.RentalDateFrom >= rentalForCreate.RentalDateTo)
+                ModelState.AddModelError("RentalDateFrom&RentalDateTo", "Error! El alquiler debe acabar más tarde de la fecha de comienzo");
+
+            if (rentalForCreate.RentalItems.Count == 0)
+                ModelState.AddModelError("RentalItems", "Error! Se debe introducir al menos un dispositivo para alquilar");
+
+            //  condiciones que pueden llevar a error en la creación del alquiler
+
+            if (ModelState.ErrorCount > 0) return BadRequest(new ValidationProblemDetails(ModelState)); //Si hay errores, la entrada es incorrecta
+
+            var deviceModel = rentalForCreate.RentalItems.Select(ri => ri.NameModel).ToList<string>(); //Se obtienen los modelos de los dispositivos a alquilar
+
+            var devices = await _context.Device
+                .Include(d => d.Model)
+                .Include(d => d.RentedDevices)
+                    .ThenInclude(rd => rd.Rental)
+                .Where(d => deviceModel.Contains(d.Model.NameModel))
+
+                .Select(d => new
+                {
+                    d.Id,
+                    d.Model.NameModel,
+                    d.Brand,
+                    d.priceForRent,
+                    d.quantityForRent,
+                    NumberOfRentedItems = d.RentedDevices
+                        .Where(rd =>
+                            (rd.Rental.RentalDateFrom < rentalForCreate.RentalDateTo) &&
+                            (rd.Rental.RentalDateTo > rentalForCreate.RentalDateFrom))
+                        .Sum(rd => rd.Quantity)
+                })
+                .ToListAsync();
+
+            //Creación del alquiler
+            Rental rental = new Rental(rentalForCreate.DeliveryAddress, DateTime.Today, rentalForCreate.PaymentMethod, rentalForCreate.RentalDateFrom, rentalForCreate.RentalDateTo, new List<RentDevice>(), user);
+
+            var numDays = (rental.RentalDateTo - rental.RentalDateFrom).TotalDays;
+
+            foreach (var item in rentalForCreate.RentalItems)
+            {
+                //Buscar por Modelo Y Marca
+                var device = devices.FirstOrDefault(d => d.NameModel == item.NameModel && d.Brand == item.Brand);
+
+                //Check de que hay cantidad adecuada
+                if ((device == null) || (item.Quantity > device.quantityForRent)) // Otra prueba
+                {
+                    ModelState.AddModelError("RentalItems", $"Error! Campos Modelo: '{item.NameModel}' Marca: '{item.Brand}' vacíos o se supera cantidad disponible");
+                }
+                else
+                {
+                    var rentDevice = new RentDevice
+                    {
+                        DeviceId = device.Id,
+                        Price = device.priceForRent,
+                        Quantity = item.Quantity,
+                        RentalId = rental.Id
+                    };
+                    rental.RentDevices.Add(rentDevice);
+                    item.priceForRent = device.priceForRent;
+                }
+            }
+
+            //Precio total
+            rental.TotalPrice = rental.RentDevices.Sum(ri => ri.Price * ri.Quantity * numDays);
+
+            //Si hay problemas con la cantidad o que el dispositivo no existe...
+            if (ModelState.ErrorCount > 0)
+            {
+                return BadRequest(new ValidationProblemDetails(ModelState));
+            }
+
+            _context.Add(rental);
+
+            try
+            {
+                //we store in the database both rental and its rentalitems
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex.Message);
+                ModelState.AddModelError("Rental", $"Error! There was an error while saving your rental, plese, try again later"); //Otra prueba
+                return Conflict("Error" + ex.Message);
+
+            }
+
+            var rentalDetail = new RentalDetailDTO(rental.ApplicationUser.Name, rental.ApplicationUser.Surname, rental.DeliveryAddress, rental.RentalDate, rental.TotalPrice, rental.RentalDateFrom, rental.RentalDateTo, rentalForCreate.RentalItems);
+
+            return CreatedAtAction("GetRentalDetail", new { id = rental.Id }, rentalDetail);
+
+        }
     }
 }
