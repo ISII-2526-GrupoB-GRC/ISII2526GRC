afff2f8f51225f78aee95b570db4fa58a3db8ad7
Rodrigo Díaz Quintanar <rodrigo.diaz7@alu.uclm.es>
2025-11-13 19:10:05 +0100

    Implementar get del detalle para el caso de uso contratar reparacion (#127)
    Controlador realizado

---
+++ src/AppForSEII2526.API/Controllers/ReceiptsController.cs
@@ -0,0 +1,59 @@
+﻿using AppForSEII2526.API.DTOs.ReceiptDTOs;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReceiptsController : ControllerBase
+    {
+        //used to enable your controller to access to the database 
+        private readonly ApplicationDbContext _context;
+        //used to log any information when your system is running
+        private readonly ILogger<ReceiptsController> _logger;
+
+        public ReceiptsController(ApplicationDbContext context, ILogger<ReceiptsController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(ReceiptDetailDTO), (int)HttpStatusCode.OK)]
+        [ProducesResponseType((int)HttpStatusCode.NotFound)]
+        public async Task<ActionResult> GetReceipt(int id)
+        {
+            if (_context.Receipt == null)
+            {
+                _logger.LogError("Error: Receipts table does not exist");
+                return NotFound();
+            }
+            var receipt = await _context.Receipt
+                .Where(r => r.Id == id)
+                    .Include(r => r.ApplicationUser)
+                    .Include(r => r.ReceiptItems)
+                        .ThenInclude(ri => ri.Repair)
+                            .ThenInclude(rep => rep.Scale)
+                .Select(r => new ReceiptDetailDTO(
+                    r.Id, r.ApplicationUser.Name, r.ApplicationUser.Surname, r.deliveryAddres, r.ReceiptDate, r.TotalPrice
+                    , r.ReceiptItems.Select(ri => new ReceiptItemDTO(
+                        ri.Repair.Name,
+                        ri.Repair.Scale.Name,
+                        ri.Repair.Cost,
+                        ri.Model
+                    )).ToList<ReceiptItemDTO>()
+                ))
+                .FirstOrDefaultAsync();
+            if (receipt == null)
+            {
+                _logger.LogWarning($"Error: Rental with id {id} does not exist");
+                return NotFound();
+            }
+            return Ok(receipt);
+        }
+
+
+    }
+}
