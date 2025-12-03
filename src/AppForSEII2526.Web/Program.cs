3e2ca27b41a7cd578f755e7d895ab81d11cdabde
rodrivva <rdiazquintanar@gmail.com>
2025-12-03 18:20:39 +0100

    Añadido StateContainer al builder

--- src/AppForSEII2526.Web/Program.cs
+++ src/AppForSEII2526.Web/Program.cs
@@ -4,6 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using AppForSEII2526.Web.Components;
 using AppForSEII2526.Web.Components.Account;
 using AppForSEII2526.Web.Data;
+using AppForSEII2526.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -34,6 +35,9 @@ builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.Requ
     .AddDefaultTokenProviders();
 
 builder.Services.AddSingleton<IEmailSender<ApplicationUser>, IdentityNoOpEmailSender>();
+
+builder.Services.AddScoped<ReceiptStateContainer>();
+
 
 var app = builder.Build();
 
