f58b906d6993f27a14a44280ce5da64bb2822c07
rodrivva <rdiazquintanar@gmail.com>
2025-12-04 19:21:36 +0100

    Ahora se hace uso de la API client en el StateContainer

--- src/AppForSEII2526.Web/ReceiptStateContainer.cs
+++ src/AppForSEII2526.Web/ReceiptStateContainer.cs
@@ -1,53 +1,58 @@
-﻿using AppForSEII2526.API.DTOs.ReceiptDTOs;
-using AppForSEII2526.API.Models;
-using AppForSEII2526.Web.API;
+﻿using AppForSEII2526.Web.API;
 
 namespace AppForSEII2526.Web
 {
     public class ReceiptStateContainer
     {
-        public ReceiptForCreateDTO Receipt { get; private set; } = new ReceiptForCreateDTO() {
+        public ReceiptForCreateDTO Receipt { get; private set; } = new ReceiptForCreateDTO()
+        {
             //receiptItems = new List<ReceiptItemForCreateDTO>();    No hace falta porque ya lo hace el constructor por defecto
         };
 
         public event Action? OnChange;
-        
+
         private void NotifyStateChanged() => OnChange?.Invoke();
 
-        public decimal TotalCost()
+        public double TotalCost()
         {
-            decimal total = 0;
-            foreach (var item in Receipt.receiptItems)
+            double total = 0;
+            foreach (var item in Receipt.ReceiptItems)
             {
                 total += item.Cost;
             }
             return total;
         }
 
-        public void AddReceiptItem(ReceiptItemDTO item) {
+        public void AddReceiptItem(RepairDTO item)
+        {
             //Antes de añadir un item comprobamos si ya está añadido
-            if (!Receipt.receiptItems.Any(ri => ri.Name == item.Name))
+            if (!Receipt.ReceiptItems.Any(ri => ri.Name == item.Name))
             {
-                Receipt.receiptItems.Add(new ReceiptItemDTO() {
+                Receipt.ReceiptItems.Add(new ReceiptItemDTO()
+                {
                     Name = item.Name,
-                    Scale = item.Scale,
+                    Scale = item.ScaleName,
                     Cost = item.Cost,
-                    Model = item.Model
+                    Model = item.Description //Preguntar a Aurora sí esto es correcto
                 });
                 NotifyStateChanged();
             }
         }
 
-        public void RemoveReceiptItem(ReceiptItemDTO item) { 
-            Receipt.receiptItems.Remove(item);
+        public void RemoveReceiptItem(ReceiptItemDTO item)
+        {
+            Receipt.ReceiptItems.Remove(item);
         }
 
-        public void ClearReceipt() {
-            Receipt.receiptItems.Clear();
+        public void ClearReceipt()
+        {
+            Receipt.ReceiptItems.Clear();
         }
 
-        public void ReceiptProcessed() { 
-            Receipt = new ReceiptForCreateDTO() {
+        public void ReceiptProcessed()
+        {
+            Receipt = new ReceiptForCreateDTO()
+            {
                 //receiptItems = new List<ReceiptItemForCreateDTO>();    No hace falta porque ya lo hace el constructor por defecto
             };
         }
