71451f1442303923aa22346e90e65e37335a5eff
rodrivva <rdiazquintanar@gmail.com>
2025-12-11 23:31:08 +0100

    Creado el PO de SelectRepairs y su UIT

---
+++ test/AppForSEII2526.UIT/UC_Receipt/SelectRepairs_PO.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Receipt
+{
+    public class SelectRepairs_PO : PageObject
+    {
+        By inputName = By.Id("inputName");
+        By inputScale = By.Id("inputScale");
+        By buttonSearchRepairs = By.Id("searchRepairs");
+        By tableOfRepairsBy = By.Id("TableofRepairs");
+        public SelectRepairs_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) { }
+        public void SearchMovies(string name, string scale) {
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).SendKeys(name);
+            
+
+            //En el proyecto de la asignatura se hizo Elena un controlador para los géneros disponibles que se podría aplicar aquí con las escalas disponibles. Preguntar sí es necesario y sí en caso de no tenerlo baja nota
+            WaitForBeingClickable(inputScale);
+            _driver.FindElement(inputScale).SendKeys(scale);
+
+            _driver.FindElement(buttonSearchRepairs).Click();
+        }
+        public bool CheckListOfRepairs(List<String[]> expectedRepairs) 
+        {
+            return CheckBodyTable(expectedRepairs, tableOfRepairsBy);
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_Receipt/UC_Receipts_UIT.cs
@@ -0,0 +1,23 @@
+﻿using AppForMovies.UIT.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Receipt
+{
+    public class UC_Receipts_UIT : UC_UIT
+    {
+        private SelectRepairs_PO selectRepairs_PO;
+        public UC_Receipts_UIT(ITestOutputHelper output) : base(output)
+        {
+
+        }
+        /* Como no va aún el login lo dejo comentado 
+        private void Precondition_perform_login() {
+            Perform_login("rdiaz@example.com", "Password123!");
+        }
+        */
+    }
+}
