bc2d7595bf62a1f8a4d70cf454ac3fe52c6d5d46
GuillermoRS05 <guillermo.rosillo@alu.uclm.es>
2025-12-12 14:05:35 +0100

    Creado ModelsController

---
+++ src/AppForSEII2526.API/Controllers/ModelsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppForSEII2526.API.Controllers
+{
+    public class ModelsController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+        private ILogger _logger;
+
+        private ModelsController(ApplicationDbContext context, ILogger logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
+
+        // Get: api/Devices/GetDevicesForRental
+        [HttpGet]
+        [Route("[action]")]
+        [ProducesResponseType(typeof(IList<string>), (int)HttpStatusCode.OK)]
+        public async Task<IActionResult> GetModelsForRental(string? modelName) // ? significa que es opcional el parámetro
+        {
+            var models = await _context.Modelo
+                .Where(model => (modelName == null || model.NameModel.Contains(modelName)))
+                .OrderBy(model => model.NameModel)
+                .Select(model => model.NameModel)
+                .ToListAsync();
+            return Ok(models);
+        }
+    }
+}
