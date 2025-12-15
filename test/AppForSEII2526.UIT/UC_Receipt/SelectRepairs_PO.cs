767408661a68e1084d9f28fdc2dd8a46bf95dcc5
Rodrigo Díaz Quintanar <rodrigo.diaz7@alu.uclm.es>
2025-12-15 16:51:10 +0100

    Pruebas funcionales select reparaciones (#250)
    * Actualizado Selenium a la última versión y cambiada URL para que coincida con la de la web
    
    * Creado el PO de SelectRepairs y su UIT
    
    * Prueba de los filtros hecha
    
    * Cambiado appsetting para que coja las cosas de la api local y erinadas pruebas del select
    
    * empezada prueba flujo básico
    
    * Terminadas las pruebas funcionales del select

---
+++ test/AppForSEII2526.UIT/UC_Receipt/SelectRepairs_PO.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Receipt
+{
+    public class SelectRepairs_PO : PageObject
+    {
+        By inputName = By.Id("inputName");
+        By inputScale = By.Id("inputScale");
+        By buttonSearchRepairs = By.Id("searchRepairs");
+        By RepairButton = By.Id("RepairButton");
+        By tableOfRepairsBy = By.Id("TableofRepairs");
+        public SelectRepairs_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output) { }
+        public void SearchMovies(string name, string scale) {
+            WaitForBeingClickable(inputName);
+            _driver.FindElement(inputName).SendKeys(name);
+            
+
+            //En el proyecto de la asignatura se hizo Elena un controlador para los géneros disponibles que se podría aplicar aquí con las escalas disponibles. Preguntar sí es necesario y sí en caso de no tenerlo baja nota
+            WaitForBeingClickable(inputScale);
+            _driver.FindElement(inputScale).SendKeys(scale);
+
+            _driver.FindElement(buttonSearchRepairs).Click();
+        }
+        public bool CheckListOfRepairs(List<String[]> expectedRepairs) 
+        {
+            return CheckBodyTable(expectedRepairs, tableOfRepairsBy);
+        }
+        public void AddRepairToReceipt(string repairName) 
+        {
+            WaitForBeingClickable(By.Id("repair_"+repairName));
+            _driver.FindElement(By.Id("repair_" + repairName)).Click();
+        }
+        public void RemoveRepairFromReceipt(string repairName) 
+        {
+            WaitForBeingClickable(By.Id("removeRepair_"+repairName));
+            _driver.FindElement(By.Id("removeRepair_" + repairName)).Click();
+        }
+        public void DoReceipt() 
+        {
+            WaitForBeingClickable(RepairButton);
+            _driver.FindElement(RepairButton).Click();
+        }
+        public bool ReceiptNotAvaible() { 
+            return _driver.FindElement(RepairButton).Displayed == false;
+        }
+        public bool CheckMessageErrorNotAviableRepairs(string expectedMessage) { 
+            return _driver.PageSource.Contains(expectedMessage);
+        }
+    }
+}
---
+++ test/AppForSEII2526.UIT/UC_Receipt/UC_Receipts_UIT.cs
@@ -0,0 +1,105 @@
+﻿using AppForMovies.UIT.Shared;
+using OpenQA.Selenium.DevTools.V141.DOM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_Receipt
+{
+    public class UC_Receipts_UIT : UC_UIT
+    {
+        private SelectRepairs_PO selectRepairs_PO;
+        private const int RepairId1 = 1; 
+        private const string repairName1 = "Cambio pantalla";
+        private const string repairScale1 = "Lujo";
+        private const string cost1 = "150,00 €";
+        private const string description1 = "Sustitución completa de pantalla OLED";
+        
+
+        private const int RepairId2 = 3; 
+        private const string repairName2 = "Reparación puerto carga";
+        private const string repairScale2 = "Básica";
+        private const string cost2 = "45,00 €";
+        private const string description2 = "Reparación del puerto de carga USB-C";
+
+        private const string AddToReceipt = "Add to Receipt";
+
+        private const string name = "Rodrigo";
+        private const string username = "rdiaz";
+        private const string surname = "Díaz Quintanar";
+        private const string deliveryAddress = "Calle Prueba";
+        private const string paymentMethod1 = "Credit Card";
+        private const string paymentMethod2 = "PayPal";
+        private const string paymentMethod3 = "Cash";
+        private const string modelo = "Pixel 8a";
+
+        public UC_Receipts_UIT(ITestOutputHelper output) : base(output)
+        {
+            selectRepairs_PO = new SelectRepairs_PO(_driver, _output);
+        }
+        /* Como no va aún el login lo dejo comentado 
+        private void Precondition_perform_login() {
+            Perform_login("rdiaz@example.com", "Password123!");
+        }
+        */
+        private void InitialStepsForReceiptUC() 
+        {
+            Initial_step_opening_the_web_page();
+            //Precondition_perform_login();
+            selectRepairs_PO.WaitForBeingVisible(By.Id("CreateReceipt"));
+            _driver.FindElement(By.Id("CreateReceipt")).Click();
+        }
+
+        [Theory]
+        [InlineData(repairName1, username, name, surname, deliveryAddress, paymentMethod1, modelo)]
+        [InlineData(repairName1, username, name, surname, deliveryAddress, paymentMethod2, modelo)]
+        [InlineData(repairName1, username, name, surname, deliveryAddress, paymentMethod3, modelo)]
+        [Trait("Level Testing", "Funcional Testing")]
+        public void IC1_CU1_2_3_BasicFlow(string repair, string username, string name , string surname, string deliveryaddress, string paymentmethod, string model) {
+            InitialStepsForReceiptUC();
+
+            var expectedReceiptItems = new List<string[]> { 
+                new string[]{ repairName1, repairScale1, cost1, modelo},
+            };
+
+
+        }
+        [Theory]
+        [InlineData(repairName1, repairScale1, cost1, description1,AddToReceipt,"Cambio pan", "")]
+        [InlineData(repairName2, repairScale2,cost2, description2, AddToReceipt, "", "Bá")]
+        [Trait("Level Testing", "Funcional Testing")]
+        public void UC4_AF1_UC5_6_filtering(string repairName, string repairScale, string RepairCost, string RepairDescription, string AddToReceipt,string filterName, string filterScale) //En el gihub de elena tiene otro nombre pero no sé bien que es. Preguntar a Aurora.
+        {
+            //Arrange
+            InitialStepsForReceiptUC();
+            var expectedRepairs = new List<string[]>
+            {
+                new string[] { repairName, repairScale,  RepairCost, RepairDescription, AddToReceipt},
+                //new String[] { RepairId2.ToString(), repairName2, repairScale2}
+            };
+            //Act
+            Thread.Sleep(4000); //Esperamos a que cargue la página
+            selectRepairs_PO.SearchMovies(filterName, filterScale);
+            Thread.Sleep(4000); //Esperamos a que cargue la tabla con los resultados
+            //Assert
+            Assert.True(selectRepairs_PO.CheckListOfRepairs(expectedRepairs));
+        }
+
+        [Fact]
+        [Trait("LevelTesting", "Funcional Testing")]
+        public void UC1_AF2_UC1_8_ReceiptNotAvailable() 
+        {
+            //Arrange
+            InitialStepsForReceiptUC();
+            
+            //Act
+            selectRepairs_PO.AddRepairToReceipt(repairName1);
+            selectRepairs_PO.RemoveRepairFromReceipt(repairName1);
+
+            //Assert
+            Assert.True(selectRepairs_PO.ReceiptNotAvaible());
+        }
+    }
+}
