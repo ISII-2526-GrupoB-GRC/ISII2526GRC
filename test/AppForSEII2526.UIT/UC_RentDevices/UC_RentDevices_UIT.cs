bf437bed82f8d09a76a1df49d0beb085b94ea60d
GuillermoRS05 <guillermo.rosillo@alu.uclm.es>
2025-12-15 20:14:49 +0100

    Creado Pageobject de SelectDevicesForRental y su UIT

---
+++ test/AppForSEII2526.UIT/UC_RentDevices/UC_RentDevices_UIT.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AppForSEII2526.UIT.Shared;
+
+namespace AppForSEII2526.UIT.UC_RentDevices
+{
+    public class UC_RentDevices_UIT : UC_UIT
+    {
+        private SelectDevicesForRental_PO selectDevicesForRental_PO;
+        public UC_RentDevices_UIT(ITestOutputHelper output) : base(output) { }
+
+        //Precondición: Usuario logueado
+        // private void Precondition_perform_login() { Perform_login("grosillo@example.com", "Password123!"); }
+
+    }
+}
