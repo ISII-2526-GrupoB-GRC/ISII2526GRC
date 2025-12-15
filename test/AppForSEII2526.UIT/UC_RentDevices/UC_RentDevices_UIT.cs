016bf5d8f43b44ce5ab6bc8c6acb0618e016e2d3
GuillermoRS05 <guillermo.rosillo@alu.uclm.es>
2025-12-15 20:29:07 +0100

    Añadido el id a la opción del NavMenu que queremos seleccionar.
    Añadidos pasos iniciales: login + click en Rental

--- test/AppForSEII2526.UIT/UC_RentDevices/UC_RentDevices_UIT.cs
+++ test/AppForSEII2526.UIT/UC_RentDevices/UC_RentDevices_UIT.cs
@@ -10,10 +10,21 @@ namespace AppForSEII2526.UIT.UC_RentDevices
     public class UC_RentDevices_UIT : UC_UIT
     {
         private SelectDevicesForRental_PO selectDevicesForRental_PO;
-        public UC_RentDevices_UIT(ITestOutputHelper output) : base(output) { }
+        public UC_RentDevices_UIT(ITestOutputHelper output) : base(output) 
+        {
+            Perform_login("grosillo@example.com", "Password123!");
+        }
 
         //Precondición: Usuario logueado
         // private void Precondition_perform_login() { Perform_login("grosillo@example.com", "Password123!"); }
+
+        private void InitialStepsForRentingDevices()
+        {
+            //Precondition_perform_login();
+            selectDevicesForRental_PO.WaitForBeingVisible(By.Id("CreateRental"));
+            // Click en el menu...
+            _driver.FindElement(By.Id("CreateRental")).Click();
+        }
 
     }
 }
