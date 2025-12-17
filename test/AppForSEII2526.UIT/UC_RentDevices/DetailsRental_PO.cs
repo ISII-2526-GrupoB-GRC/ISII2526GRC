b00acef5cdf7e3f248afba2ded99de627c53509f
GuillermoRS05 <guillermo.rosillo@alu.uclm.es>
2025-12-17 17:32:56 +0100

    Creado PageObject del detalle del Alquiler

---
+++ test/AppForSEII2526.UIT/UC_RentDevices/DetailsRental_PO.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppForSEII2526.UIT.UC_RentDevices
+{
+    public class DetailsRental_PO : PageObject
+    {
+        public DetailsRental_PO(IWebDriver driver, ITestOutputHelper output) : base(driver, output)
+        {
+        }
+
+        public bool CheckRentalDetail(string nameSurname, string delivery, DateTime rentalDate, double totalPrice, DateTime rentalDateFrom, DateTime rentalDateTo)
+        {
+            WaitForBeingVisible(By.Id("TotalPrice"));
+            bool result = true;
+            result = result && _driver.FindElement(By.Id("NameSurname")).Text.Contains(nameSurname);
+            result = result && _driver.FindElement(By.Id("DeliveryAddress")).Text.Contains(delivery);
+            result = result && _driver.FindElement(By.Id("TotalPrice")).Text.Contains(totalPrice.ToString());
+
+            var actualDate = DateTime.Parse(_driver.FindElement(By.Id("RentalDate")).Text);
+            result = result && actualDate.Date == rentalDate.Date;
+
+            result = result && _driver.FindElement(By.Id("RentalPeriod")).Text.Contains($"{rentalDateFrom.ToShortDateString()} - {rentalDateTo.ToShortDateString()}");
+
+            return result;
+        }
+
+        public bool CheckListOfDevices(List<string[]> expectedRentalItems)
+        {
+            return CheckBodyTable(expectedRentalItems, By.Id("RentedDevices"));
+        }
+
+    }
+}
